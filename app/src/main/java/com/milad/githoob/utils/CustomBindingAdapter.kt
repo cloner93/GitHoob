38d1cd63c2558b38f6dcff1595240bf9d4264120
Milad <targholi2112@gmail.com>
2022-01-27 22:35:15 +0330

    fix bug in profile header(createAt)

--- app/src/main/java/com/milad/githoob/utils/CustomBindingAdapter.kt
+++ app/src/main/java/com/milad/githoob/utils/CustomBindingAdapter.kt
@@ -1,8 +1,16 @@
 package com.milad.githoob.utils
 
+import android.text.format.DateUtils
+import android.util.Log
+import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import timber.log.Timber
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
 @BindingAdapter("profileImage")
 fun profileImage(
@@ -13,4 +21,29 @@ fun profileImage(
         .load(imageUrl)
         .circleCrop()
         .into(view)
+}
+
+@BindingAdapter("profile_createdAt")
+fun profileCreatedAt(
+    tvCreateAt: TextView,
+    created_at: String?
+) {
+
+    if (created_at != null) {
+        val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+        sdf.timeZone = TimeZone.getTimeZone("GMT")
+
+        try {
+            val time: Long = sdf.parse(created_at).time
+            val now = System.currentTimeMillis()
+            val ago =
+                DateUtils.getRelativeTimeSpanString(time, now, DateUtils.MINUTE_IN_MILLIS)
+
+            tvCreateAt.text = "joined :$ago"
+        } catch (e: ParseException) {
+            e.printStackTrace()
+            tvCreateAt.visibility = View.GONE
+        }
+    }
+
 }
