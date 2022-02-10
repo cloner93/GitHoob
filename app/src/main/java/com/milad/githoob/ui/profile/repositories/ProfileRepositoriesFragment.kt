11a6b27bc6debb4dc33d7717dbb5e0747072b1b3
cloner93 <targholi2112@gmail.com>
2022-02-10 23:27:51 +0330

    edit collapsable add button in repository.

--- app/src/main/java/com/milad/githoob/ui/profile/repositories/ProfileRepositoriesFragment.kt
+++ app/src/main/java/com/milad/githoob/ui/profile/repositories/ProfileRepositoriesFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
@@ -45,6 +46,25 @@ class ProfileRepositoriesFragment : Fragment() {
         userId = safeArgs.userId
 
         setupRecyclerView()
+        binding.profileRepositoriesNestedList.setOnScrollChangeListener(
+            NestedScrollView.OnScrollChangeListener { v, _, scrollY, _, oldScrollY ->
+                run {
+                    if (scrollY > oldScrollY) {
+                        binding.floatingActionButton.shrink()
+                    }
+                    if (scrollY < oldScrollY) {
+                        binding.floatingActionButton.extend()
+                    }
+
+//                    if (scrollY == 0) {
+//                        Timber.d("TOP")
+//                    }
+
+//                    if (scrollY == -(v.measuredHeight - v.getChildAt(0).measuredHeight)) {
+//                        Timber.d("Bottom $scrollY == ${v.measuredHeight - v.getChildAt(0).measuredHeight}")
+//                    }
+                }
+            })
 
         viewModel.setUser(token = token, userId = userId)
 
@@ -57,22 +77,10 @@ class ProfileRepositoriesFragment : Fragment() {
             adapter = ProfileRepositoryAdapter(viewmodel)
             binding.profileRepositoriesList.adapter = adapter
         }
-        if (token != null && token!="")
+        if (token != null && token != "")
             binding.floatingActionButton.show()
         else
             binding.floatingActionButton.hide()
-
-        binding.profileRepositoriesList.addOnScrollListener(object :
-            RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-                if (dy > 0)
-                    binding.floatingActionButton.shrink()
-                else
-                    binding.floatingActionButton.extend()
-
-            }
-        })
     }
 
 }
