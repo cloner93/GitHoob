484b96b855bc585135fc51f3aafd2245838ed14c
cloner93 <targholi2112@gmail.com>
2022-02-26 17:32:20 +0330

    add ProfileRepositoriesFragment and viewModel for project details.

---
+++ app/src/main/java/com/milad/githoob/ui/profile/repositories/project/ProfileProjectFragment.kt
@@ -0,0 +1,30 @@
+package com.milad.githoob.ui.profile.repositories.project
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.milad.githoob.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProfileProjectFragment : Fragment() {
+
+    private lateinit var viewModel: ProjectViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.profile_project_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ProjectViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/milad/githoob/ui/profile/repositories/project/ProjectViewModel.kt
@@ -0,0 +1,9 @@
+package com.milad.githoob.ui.profile.repositories.project
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class ProjectViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
