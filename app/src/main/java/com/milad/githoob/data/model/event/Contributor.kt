01500aacd0a5b07150f66fcbab3cefbd9928aedd
cloner93 <targholi2112@gmail.com>
2022-02-28 19:33:01 +0330

    edit contributors list

--- app/src/main/java/com/milad/githoob/data/model/event/Contributor.kt
+++ app/src/main/java/com/milad/githoob/data/model/event/Contributor.kt
@@ -1,23 +1,23 @@
 package com.milad.githoob.data.model.event
 
-class Contributor(
-    var login: String?,
-    var id: Int?,
-    var nodeId: String?,
-    var avatarUrl: String?,
-    var gravatarId: String?,
-    var url: String?,
-    var htmlUrl: String?,
-    var followersUrl: String?,
-    var followingUrl: String?,
-    var gistsUrl: String?,
-    var starredUrl: String?,
-    var subscriptionsUrl: String?,
-    var organizationsUrl: String?,
-    var reposUrl: String?,
-    var eventsUrl: String?,
-    var receivedEventsUrl: String?,
-    var type: String?,
-    var siteAdmin: Boolean?,
-    var contributions: Int?
+data class Contributor(
+    val login: String,
+    val id: Int,
+    val node_id: String,
+    val avatar_url: String,
+    val gravatar_id: String,
+    val url: String,
+    val html_url: String,
+    val followers_url: String,
+    val following_url: String,
+    val gists_url: String,
+    val starred_url: String,
+    val subscriptions_url: String,
+    val organizations_url: String,
+    val repos_url: String,
+    val events_url: String,
+    val received_events_url: String,
+    val type: String,
+    val site_admin: Boolean,
+    val contributions: Int
 )
