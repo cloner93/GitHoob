d116fca384817f39a9518f534d637aca75e2f2be
Targholi <www.targholi@behsazan.net>
2022-05-28 14:18:11 +0430

    blend two two color for generate color by percent and delete all background colors from CollapsibleToolbar

---
+++ app/src/main/java/com/milad/githoob/utils/GlobalState.kt
@@ -0,0 +1,9 @@
+package com.milad.githoob.utils
+
+object GlobalState {
+    const val default_percent_1: Float = 0.05F
+    const val default_percent_2: Float = 0.08F
+    const val default_percent_3: Float = 0.011F
+    const val default_percent_4: Float = 0.012F
+    const val default_percent_5: Float = 0.014F
+}
