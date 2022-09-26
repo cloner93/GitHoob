ce740333a1828e1426898f309a399af87af713e6
Targholi <www.targholi@behsazan.net>
2022-09-26 14:31:33 +0330

    remove buildSrc and replace gradle catalog.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,52 +3,35 @@ plugins {
     id("kotlin-android")
     id("kotlin-android-extensions")
     id("kotlin-kapt")
-    id("dagger.hilt.android.plugin")
-    id("androidx.navigation.safeargs.kotlin")
+    id("com.google.dagger.hilt.android")
+    id("androidx.navigation.safeargs")
 }
 
 android {
-    compileSdkVersion(AppConfig.compileSdk)
-//    buildToolsVersion(AppConfig.buildToolsVersion)
+    compileSdk = 31
 
     defaultConfig {
-        applicationId = AppConfig.applicationId
-        minSdkVersion(AppConfig.minSdk)
-        targetSdkVersion(AppConfig.targetSdk)
-        versionCode = AppConfig.versionCode
-        versionName = AppConfig.versionName
+        applicationId = "com.milad.githoob"
+        minSdk = 21
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1.0"
 
-        testInstrumentationRunner = AppConfig.androidTestInstrumentation
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-//            minifyEnabled = false
-            proguardFiles(
-                    getDefaultProguardFile("proguard-android-optimize.txt"),
-                    "proguard-rules.pro"
-            )
+            isMinifyEnabled = false
+//            proguardFiles = getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
         }
     }
-
-    flavorDimensions(AppConfig.dimension)
-    productFlavors {
-        create("staging") {
-            applicationIdSuffix = ".staging"
-            setDimension(AppConfig.dimension)
-        }
-
-        create("production") {
-            setDimension(AppConfig.dimension)
-        }
-    }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = AppConfig.jvmTarget
+        jvmTarget = "1.8"
     }
     buildFeatures {
         viewBinding = true
@@ -57,10 +40,45 @@ android {
 }
 
 dependencies {
-//  implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation(AppDependencies.appLibraries)
-    kapt(AppDependencies.kaptLibraries)
-//    annotationProcessor(AppDependencies.annotationLibraries)
-    testImplementation(AppDependencies.testLibraries)
-    androidTestImplementation(AppDependencies.androidTestLibraries)
+
+    implementation(libs.coreKtx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintLayout)
+    implementation(libs.navigationFragment)
+    implementation(libs.navigationUi)
+    implementation(libs.coroutinesCore)
+    implementation(libs.coroutinesAndroid)
+    implementation(libs.legacy)
+    implementation(libs.recyclerview)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.extJUnit)
+    androidTestImplementation(libs.espressoCore)
+
+    implementation(libs.lifecycleViewmodel)
+    implementation(libs.lifecycleRuntime)
+    implementation(libs.lifecycleLivedata)
+    implementation(libs.lifecycleExtensions)
+
+    implementation(libs.activity)
+    implementation(libs.lifecycleArch)
+    implementation(libs.gson)
+    implementation(libs.retrofit)
+    implementation(libs.retrofitGson)
+    implementation(libs.okhttpLogging)
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+    kapt(libs.hilt.ext.compiler)
+//    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0")
+//    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+
+    implementation(libs.glide)
+    implementation(libs.glide.compiler)
+    implementation(libs.glide.transformations)
+    implementation(libs.datastore)
+    implementation(libs.markwon.core)
+    implementation(libs.markwon.html)
+    implementation(libs.markwon.image)
+    implementation(libs.timber)
 }
